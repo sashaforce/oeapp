97b3343345ec382a76d60578eb779d7b21f6d05b
Sasha Force <sashaforce@gmail.com>
2018-06-06 21:18:58 -0400

    added audio for samples to phonemes; missing mp3 for mothor

---
+++ src/audio-data.service.js
@@ -0,0 +1,30 @@
+(function() {
+  'use strict';
+
+angular.module('Data').service('AudioDataService', AudioDataService);
+
+AudioDataService.$inject = [];
+function AudioDataService(){
+  var svc = this;
+
+  // Array for special cases. Otherwise URL = "/audio/<id>.mp3"
+  // TODO: move to db
+  var fileArray = [];
+
+  svc.getUrl = function (id) {
+
+    console.log("AudioDataService.getUrl for id:", id);
+
+     // check exceptions list
+     for (var i=0; i < fileArray.length; i++) {
+       var f = fileArray[i];
+       if (f.id === id) {
+         return f.url;
+       }
+     }
+
+    return `/audio/${id}.mp3`;
+  }
+}
+
+}());
--- src/phoneme.component.js
+++ src/phoneme.component.js
@@ -9,11 +9,29 @@
     }
   }).controller('PhonemeController', PhonemeController);
 
-  PhonemeController.$inject = ['ImageService'];
-  function PhonemeController(ImageService) {
+  PhonemeController.$inject = ['ImageService', 'AudioDataService'];
+  function PhonemeController(ImageService, AudioDataService) {
     var ctrl = this;
 
     ctrl.listenButtonUrl = ImageService.getUrl("speaker-button-sm");
     ctrl.imageUrl = ImageService.getUrl(ctrl.exercise.imageId);
+    ctrl.audioUrl = AudioDataService.getUrl(ctrl.exercise.audioId);
+
+    var audioElement = document.getElementById('sampleAudio');
+    console.log("audioElement:", audioElement);
+
+    ctrl.playExample = function () {
+      console.log("Play sample word");
+      var audioElement = document.getElementById('sampleAudio');
+      console.log("audioElement:", audioElement);
+      audioElement.play();
+
+      // var x = document.createElement("AUDIO");
+      // x.setAttribute("src","/audio/ael_Sasha.m4a");
+      // document.body.appendChild(x);
+      // x.play();
+      // var audio = new Audio('/audio/ael_Sasha.m4a');
+      // audio.play();
+    }
   };
 }());
