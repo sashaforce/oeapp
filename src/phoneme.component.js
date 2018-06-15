9c77615580d773202814aae1c3cd8573fa12288c
Sasha Force <sashaforce@gmail.com>
2018-06-14 21:16:04 -0400

    replaced audio element with AudioContext and buffer source. Messy but functional.

--- src/phoneme.component.js
+++ src/phoneme.component.js
@@ -18,22 +18,41 @@
     ctrl.imageUrl = ImageService.getUrl(ctrl.exercise.imageId);
     ctrl.audioUrl = AudioDataService.getUrl(ctrl.exercise.audioId);
 
-    var audioElement = document.getElementById('sampleAudio');
-    console.log("audioElement:", audioElement);
+    // initialize audio
+    var sampleBuffer = null;
+    var context;
+    try {
+      // Fix prefixing for blink & webkit-based browsers
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      context = new AudioContext();
+    }
+    catch(e) {
+      alert('Web Audio API is not supported in this browser');
+    }
+    loadSound(ctrl.audioUrl);
 
+    function loadSound(url) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.responseType = 'arraybuffer';
+
+      // Decode asynchronously
+      request.onload = function() {
+        context.decodeAudioData(request.response, function(buffer) {
+          console.log("context.decodeAudioData()");
+          sampleBuffer = buffer;
+        }, function(){console.log("ERROR");}); // TODO: Handle this properly
+      }
+      request.send();
+    }
 
     ctrl.playExample = function () {
       console.log("Play sample word");
-      var audioElement = document.getElementById('sampleAudio');
-      console.log("audioElement:", audioElement);
-      audioElement.play();
-
-      // var x = document.createElement("AUDIO");
-      // x.setAttribute("src","/audio/ael_Sasha.m4a");
-      // document.body.appendChild(x);
-      // x.play();
-      // var audio = new Audio('/audio/ael_Sasha.m4a');
-      // audio.play();
+      context.resume(); // in case Chrome has suspended the context
+      var source = context.createBufferSource(); // creates a sound source
+      source.buffer = sampleBuffer;              // tell the source which sound to play
+      source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+      source.start(0);                           // play the source now
     }
   };
 }());
