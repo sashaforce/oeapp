d6d2e7a0fe59d41668cf503b74c6848db1f2a353
Sasha Force <sashaforce@gmail.com>
2018-06-14 21:55:24 -0400

    added sample player component, use in phoneme component

--- src/phoneme.component.js
+++ src/phoneme.component.js
@@ -18,41 +18,5 @@
     ctrl.imageUrl = ImageService.getUrl(ctrl.exercise.imageId);
     ctrl.audioUrl = AudioDataService.getUrl(ctrl.exercise.audioId);
 
-    // initialize audio
-    var sampleBuffer = null;
-    var context;
-    try {
-      // Fix prefixing for blink & webkit-based browsers
-      window.AudioContext = window.AudioContext||window.webkitAudioContext;
-      context = new AudioContext();
-    }
-    catch(e) {
-      alert('Web Audio API is not supported in this browser');
-    }
-    loadSound(ctrl.audioUrl);
-
-    function loadSound(url) {
-      var request = new XMLHttpRequest();
-      request.open('GET', url, true);
-      request.responseType = 'arraybuffer';
-
-      // Decode asynchronously
-      request.onload = function() {
-        context.decodeAudioData(request.response, function(buffer) {
-          console.log("context.decodeAudioData()");
-          sampleBuffer = buffer;
-        }, function(){console.log("ERROR");}); // TODO: Handle this properly
-      }
-      request.send();
-    }
-
-    ctrl.playExample = function () {
-      console.log("Play sample word");
-      context.resume(); // in case Chrome has suspended the context
-      var source = context.createBufferSource(); // creates a sound source
-      source.buffer = sampleBuffer;              // tell the source which sound to play
-      source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-      source.start(0);                           // play the source now
-    }
   };
 }());
---
+++ src/sample-player.component.js
@@ -0,0 +1,58 @@
+(function() {
+  'use strict';
+
+  angular.module('OeApp').component('samplePlayerComponent', {
+    templateUrl: 'src/templates/sample-player.template.html',
+    controller: 'SamplePlayerController as ctrl',
+    bindings: {
+      url: '<',
+      oeWord: '<',
+      meWord: '<'
+    }
+  })
+  .controller('SamplePlayerController', SamplePlayerController);
+
+  SamplePlayerController.$inject = [];
+  function SamplePlayerController() {
+    var ctrl = this;
+    console.log("SamplePlayerController() START", ctrl);
+
+    // initialize audio
+    var sampleBuffer = null;
+    var context;
+    try {
+      // Fix prefixing for blink & webkit-based browsers
+      window.AudioContext = window.AudioContext||window.webkitAudioContext;
+      context = new AudioContext();
+    }
+    catch(e) {
+      alert('Web Audio API is not supported in this browser');
+    }
+    loadSound(ctrl.url);
+
+    function loadSound(url) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.responseType = 'arraybuffer';
+
+      // Decode asynchronously
+      request.onload = function() {
+        context.decodeAudioData(request.response, function(buffer) {
+          console.log("context.decodeAudioData()");
+          sampleBuffer = buffer;
+        }, function(){console.log("ERROR");}); // TODO: Handle this properly
+      }
+      request.send();
+    }
+
+    ctrl.playExample = function () {
+      console.log("Play sample word");
+      context.resume(); // in case Chrome has suspended the context
+      var source = context.createBufferSource(); // creates a sound source
+      source.buffer = sampleBuffer;              // tell the source which sound to play
+      source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+      source.start(0);                           // play the source now
+    }
+  }
+
+}());
