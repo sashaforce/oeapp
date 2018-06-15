18f4479c335dbf821eacc12d85fad6d4a40d39be
Sasha Force <sashaforce@gmail.com>
2018-06-15 12:41:35 -0400

    set user recording to play via AudioContext. Working, not pretty

--- src/record-exercise.component.js
+++ src/record-exercise.component.js
@@ -19,21 +19,41 @@ function RecordExerciseController (AudioDataService) {
   ctrl.isRecording = false;
   ctrl.recordButtonText = "Record";
   ctrl.sampleAudioUrl = AudioDataService.getUrl(ctrl.exercise.audioId);
-  
+
   console.log("Initializing Audio");
-  initAudio();
+  var sampleBuffer = null;
+  var context;
+  try {
+    // Fix prefixing for blink & webkit-based browsers
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    context = new AudioContext();
+  }
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
+  initAudio(); // TODO rename to reflect this is about the recorder
 
   var audioContext = new AudioContext();
 
   const audio = document.querySelector('audio');
   const recordButton = document.querySelector('#record-button');
 
-  ctrl.playSample = function () {
-    console.log("playSample() START");
-    audio.play();
+  // ctrl.playSample = function () {
+  //   console.log("playSample() START");
+  //   audio.play();
+  // }
+
+
+  ctrl.playRecording = function () {
+    console.log("Play Recording");
+    context.resume(); // in case Chrome has suspended the context
+    var source = context.createBufferSource(); // creates a sound source
+    source.buffer = sampleBuffer;              // tell the source which sound to play
+    source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+    source.start(0);                           // play the source now
   }
 
-  ctrl.toggleRecord = function () {
+  ctrl.toggleRecord = function () { // TODO: change to state machine
     if (ctrl.isRecording) {
       // stop recording
       audioRecorder.stop();
@@ -99,7 +119,23 @@ function RecordExerciseController (AudioDataService) {
   function doneEncoding( blob ) {
     //Recorder.setupDownload( blob, "myRecording.wav" );
     var url = (window.URL || window.webkitURL).createObjectURL(blob);
-    audio.src = url;
+    audio.src = url; // for sample player component, loaded from existing mp3, now loading from fresh recording
+    loadSound(url);
+  }
+
+  function loadSound(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    // Decode asynchronously
+    request.onload = function() {
+      context.decodeAudioData(request.response, function(buffer) {
+        console.log("context.decodeAudioData()");
+        sampleBuffer = buffer;
+      }, function(){console.log("ERROR");}); // TODO: Handle this properly
+    }
+    request.send();
   }
 }
 
