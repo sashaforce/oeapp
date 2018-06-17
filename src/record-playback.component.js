d654a6ca9cb9c191e797f17bd8d50a668830e216
Sasha Force <sashaforce@gmail.com>
2018-06-16 20:31:45 -0400

    re-implemented record-playback state machine using template

--- src/record-playback.component.js
+++ src/record-playback.component.js
@@ -12,47 +12,17 @@
     var ctrl = this;
 
     // init image urls
-    var speakerImage = ImageService.getUrl('speaker-button-lg');
-    var micImage = ImageService.getUrl('microphone-button');
-    var activeMicImage = ImageService.getUrl('microphone-button-active');
-    var activeSpeakerImage = ImageService.getUrl('speaker-button-active');
+    ctrl.micImage = ImageService.getUrl('microphone-button');
+    ctrl.activeMicImage = ImageService.getUrl('microphone-button-active');
+    ctrl.speakerImage = ImageService.getUrl('speaker-button-lg');
+    ctrl.activeSpeakerImage = ImageService.getUrl('speaker-button-active');
+
     // init state
     ctrl.state = 'READY';
-    ctrl.buttonImage = micImage;
 
     ctrl.changeState = function (toState) {
       // set new state
-      ctrl.state = calculateNewState(toState); // TODO does this work if null?
-      // update UI
-      if (ctrl.state === 'READY'){
-        ctrl.buttonImage = micImage;
-      } else if (ctrl.state === 'RECORDING') {
-        ctrl.buttonImage = activeMicImage;
-      } else if (ctrl.state === 'DONE') {
-        ctrl.buttonImage = speakerImage;
-      } else if (ctrl.state === 'LISTENING') {
-        ctrl.buttonImage = activeSpeakerImage;
-      }
-    }
-
-    function calculateNewState(toState) {
-      if (toState) {
-        return toState;
-      }
-      if (ctrl.state === 'READY') {
-        return 'RECORDING';
-      }
-      if (ctrl.state === 'RECORDING') {
-        return 'DONE';
-      }
-      if (ctrl.state === 'DONE') {
-        return 'LISTENING';
-      }
-      if (ctrl.state === 'LISTENING') {
-        return 'DONE';
-      }
-      console.log("calculateNewState() ERROR Unexpected state:", toState); // TODO: null OK?
-      return 'READY';
+      ctrl.state = toState;
     }
 
   };
