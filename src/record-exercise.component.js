157f9fa8f12e1c82ff321c06e5fe4c2acf8ae093
Sasha Force <sashaforce@gmail.com>
2018-06-17 15:00:23 -0400

    cleanup unused code

--- src/record-exercise.component.js
+++ src/record-exercise.component.js
@@ -14,14 +14,12 @@ function RecordExerciseController (AudioDataService, $scope) {
 
   var ctrl = this;
   ctrl.sampleAudioUrl = AudioDataService.getUrl(ctrl.exercise.audioId);
-  //ctrl.okToContinue = false;
 
   // continue should be disabled until we expressly enable it
   $scope.$emit("lesson:enableContinue", false);
 
   ctrl.enableContinue = function (enable) {
     console.log("enableContinue()", enable);
-    //ctrl.okToContinue = enable;
     $scope.$emit("lesson:enableContinue", {okToContine:enable});
   }
 }
